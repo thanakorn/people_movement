a129fa495750bc8a2c322296add63fdff9b6871d
Panyapiang, Thanakorn (Agoda) <thanakorn.panyapiang@agoda.com>
2016-09-21 20:51:30 +0700

    Add more unites

--- src/test/scala/SimpleMeetingFinderSpec.scala
+++ src/test/scala/SimpleMeetingFinderSpec.scala
@@ -11,41 +11,25 @@ class SimpleMeetingFinderSpec extends FlatSpec with Matchers {
 
   val meetingFinder = new SimpleMeetingFinder(distance, 2.0)
 
-  "SimpleMeetingFinder" should "return empty for traces with different timestamp" in {
+  "SimpleMeetingFinder" should "when t1 and t2 timestamp not overlap, return empty" in {
     val uid1 = "uid1"
-    val t1 = List(
-      Trace(uid1, 0, Location(1.0, 1.0, 1)),
-      Trace(uid1, 1, Location(1.0, 1.0, 1))
-    )
-
     val uid2 = "uid2"
-    val t2 = List(
-      Trace(uid2, 10000, Location(1.0, 1.0, 1)),
-      Trace(uid2, 11000, Location(1.0, 1.0, 1))
-    )
-
+    val t1 = List(Trace(uid1, 0, Location(1.0, 1.0, 1)), Trace(uid1, 1, Location(1.0, 1.0, 1)))
+    val t2 = List(Trace(uid2, 10000, Location(1.0, 1.0, 1)), Trace(uid2, 11000, Location(1.0, 1.0, 1)))
     meetingFinder.findMeeting(t1, t2).size should be(0)
   }
 
-  "SimpleMeetingFinder" should "return empty for traces with different floor" in {
+  "SimpleMeetingFinder" should "when t1 and t2 has different floor, return empty" in {
     val uid1 = "uid1"
-    val t1 = List(
-      Trace(uid1, 0, Location(1.0, 1.0, 1)),
-      Trace(uid1, 1, Location(1.0, 1.0, 1))
-    )
-
     val uid2 = "uid2"
-    val t2 = List(
-      Trace(uid2, 0, Location(1.0, 1.0, 2)),
-      Trace(uid2, 1, Location(1.0, 1.0, 3))
-    )
-
+    val t1 = List(Trace(uid1, 0, Location(1.0, 1.0, 1)), Trace(uid1, 1, Location(1.0, 1.0, 1)))
+    val t2 = List(Trace(uid2, 0, Location(1.0, 1.0, 2)), Trace(uid2, 1, Location(1.0, 1.0, 3)))
     meetingFinder.findMeeting(t1, t2).size should be(0)
   }
 
-  "SimpleMeetingFinder" should "return meeting list between t1 and t2 if exist" in {
-
+  "SimpleMeetingFinder" should "when t1 and t2 has same timestamp and meeting exist, return meeting list" in {
     val uid1 = "20bcf0ad"
+    val uid2 = "da0fcb02"
     val t1 = List(
       Trace(timestamp = 1, location = Location(x = 1.0, y = 1.0, floor = 2), uid = uid1),
       Trace(timestamp = 2, location = Location(x = 1.5, y = 1.0, floor = 2), uid = uid1),
@@ -55,7 +39,6 @@ class SimpleMeetingFinderSpec extends FlatSpec with Matchers {
       Trace(timestamp = 6, location = Location(x = 4.0,	y = 3.0, floor = 2), uid = uid1)
     )
 
-    val uid2 = "da0fcb02"
     val t2 = List(
       Trace(timestamp = 1, location = Location(x = 1.0, y = 5.0, floor = 2), uid = uid2),
       Trace(timestamp = 2, location = Location(x = 1.0, y = 4.0, floor = 2), uid = uid2),
@@ -69,6 +52,29 @@ class SimpleMeetingFinderSpec extends FlatSpec with Matchers {
     meetings.size should be(2)
     meetings(0) should be(Meeting(5, uid1, Location(x = 3.0, y	= 3.0, floor =	2), uid2, Location(x = 3.2, y	= 3.0, floor =	2)))
     meetings(1) should be(Meeting(6, uid1, Location(x = 4.0, y	= 3.0, floor =	2), uid2, Location(x = 3.5, y	= 2.0, floor =	2)))
+  }
+
+  "SimpleMeetingFinder" should "when t1 and t2 timestamp overlap and meeting exist, return meeting list in overlap period" in {
+    val uid1 = "20bcf0ad"
+    val uid2 = "da0fcb02"
+    val t1 = List(
+      Trace(timestamp = 1, location = Location(x = 0.5, y = 4.0, floor = 1), uid = uid1),
+      Trace(timestamp = 2, location = Location(x = 1.2, y = 3.5, floor = 1), uid = uid1),
+      Trace(timestamp = 3, location = Location(x = 2.5, y = 2.5, floor = 1), uid = uid1),
+      Trace(timestamp = 4, location = Location(x = 3.0,	y = 2.5, floor = 1), uid = uid1),
+      Trace(timestamp = 5, location = Location(x = 4.0, y	= 3.5, floor = 1), uid =	uid1),
+      Trace(timestamp = 6, location = Location(x = 4.2,	y = 3.7, floor = 1), uid = uid1)
+    )
+
+    val t2 = List(
+      Trace(timestamp = 3, location = Location(x = 2.7, y = 5.0, floor = 1), uid = uid2),
+      Trace(timestamp = 4, location = Location(x = 3.7,	y = 5.0, floor = 1), uid = uid2),
+      Trace(timestamp = 5, location = Location(x = 4.2, y	= 3.3, floor = 1), uid =	uid2)
+    )
+
+    val meetings = meetingFinder.findMeeting(t1, t2)
+    meetings.size should be(1)
+    meetings(0) should be(Meeting(5, uid1, Location(x = 4.0, y = 3.5, floor =	1), uid2, Location(x = 4.2, y	= 3.3, floor = 1)))
   }
 
 }
