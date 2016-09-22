29fb87eb921299a54d927d1512a6b469d4aadfb8
Panyapiang, Thanakorn (Agoda) <thanakorn.panyapiang@agoda.com>
2016-09-23 02:32:32 +0700

    Fix timestamp error

--- src/main/scala/output/FileResultGenerator.scala
+++ src/main/scala/output/FileResultGenerator.scala
@@ -11,7 +11,7 @@ class FileResultGenerator(filename: String) extends ResultGenerator{
     val bw = new BufferedWriter(new FileWriter(file))
     bw.write("timestamp,uid1,x1,y1,uid2,x2,y2,floor\n")
     meetings.map(m => {
-      bw.write(s"${new DateTime(m.timestamp * 1000).toString("yyyy-MM-dd'T'HH:mm:ss.SSS")},${m.uid1},${m.location1.x},${m.location1.y},${m.uid2},${m.location2.x},${m.location2.y},${m.location1.floor}\n")
+      bw.write(s"${new DateTime(m.timestamp).toString("yyyy-MM-dd'T'HH:mm:ss.SSS")},${m.uid1},${m.location1.x},${m.location1.y},${m.uid2},${m.location2.x},${m.location2.y},${m.location1.floor}\n")
     })
     bw.close()
   }
