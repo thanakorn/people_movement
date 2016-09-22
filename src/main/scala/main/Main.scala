16126c0365d957b1bd5ae12e23fddeacf6014ef5
Panyapiang, Thanakorn (Agoda) <thanakorn.panyapiang@agoda.com>
2016-09-22 23:21:21 +0700

    Start construct flow

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -1,9 +1,26 @@
 package main
 
-object Main {
+import java.lang.Math._
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, ClosedShape}
+import akka.stream.scaladsl.{Flow, GraphDSL, RunnableGraph, Sink, Source}
+import input.FileLoader
+import models._
+import preprocessing.Preprocessor
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object Main extends Preprocessor{
+
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
 
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    val flow = RunnableGraph.fromGraph(GraphDSL.create(){implicit builder =>
+      import GraphDSL.Implicits._
+      ClosedShape
+    })
+    flow.run()
   }
 
 }
